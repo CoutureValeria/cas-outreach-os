c35be5d7e9a63f09f7392245437037c9cd68ac57
CoutureValeria <couturevaleria@gmail.com>
2026-06-16 20:58:23 +0200

    chore: trigger Railway redeploy (linkedin find-all fix + model update)

--- outreach-os/server.js
+++ outreach-os/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const express = require('express');
+const express = require('express'); // v2026-06-16
 const helmet = require('helmet');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
